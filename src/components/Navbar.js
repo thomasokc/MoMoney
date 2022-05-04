a3514aad579ee3ccf4801eb1e63e42c3c2979d9d
Thomas Morris <thomasmorris682@gmail.com>
2022-05-04 11:20:21 -0500

    deployed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
     return (
         <nav className={styles.navbar}>
             <ul>
-                <li className={styles.title}>MoMoney</li>
+                <li className={styles.title}>MoMoneyApp</li>
 
                 {!user && (
                     <>
